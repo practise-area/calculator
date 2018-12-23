889e4ac367743653632db1df83964b2d130fc219
Ian Kahwai <iankahwai@gmail.com>
2018-12-23 11:27:47 +0200

    Button Re arrangement

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ export default class App extends Component{
           this.calculate();
       } else if (button === "C"){
           this.reset();
-      }else if(button === "CE"){
+      }else if(button === "DEL"){
           this.backSpace();
       }else{
           this.setState({
@@ -64,9 +64,8 @@ export default class App extends Component{
       const { result } = this.state;
       return(
           <div className="calculator-body">
-              <h1>Simple calculator</h1>
+              <h1>Calculator</h1>
               <Result result={result}/>
-              {console.log(result)}
               <KeyArea onClick={this.onClick} />
           </div>
       );
@@ -75,7 +74,7 @@ export default class App extends Component{
 const Result = ({ result }) =>{
   return (
       <div className="result">
-          <p>{result}</p>
+          <p>{(result === "")?0:result}</p>
       </div>
   );
 };
@@ -83,30 +82,37 @@ const Result = ({ result }) =>{
 const KeyArea = ({ onClick }) => {
   return (
       <div className="button">
+          <button name="DEL" onClick={e => onClick(e.target.name)}>DEL</button>
           <button name="(" onClick={e => onClick(e.target.name)}>(</button>
-          <button name="CE" onClick={e => onClick(e.target.name)}>CE</button>
           <button name=")" onClick={e => onClick(e.target.name)}>)</button>
           <button name="C" onClick={e => onClick(e.target.name)}>C</button><br/>
-
-          <button name="1" onClick={e => onClick(e.target.name)}>1</button>
-          <button name="2" onClick={e => onClick(e.target.name)}>2</button>
-          <button name="3" onClick={e => onClick(e.target.name)}>3</button>
-          <button name="+" onClick={e => onClick(e.target.name)}>+</button><br/>
-
-          <button name="4" onClick={e => onClick(e.target.name)}>4</button>
-          <button name="5" onClick={e => onClick(e.target.name)}>5</button>
-          <button name="6" onClick={e => onClick(e.target.name)}>6</button>
-          <button name="-" onClick={e => onClick(e.target.name)}>-</button><br/>
 
           <button name="7" onClick={e => onClick(e.target.name)}>7</button>
           <button name="8" onClick={e => onClick(e.target.name)}>8</button>
           <button name="9" onClick={e => onClick(e.target.name)}>9</button>
+          <button name="/" onClick={e => onClick(e.target.name)}>÷</button><br/>
+
+          <button name="4" onClick={e => onClick(e.target.name)}>4</button>
+          <button name="5" onClick={e => onClick(e.target.name)}>5</button>
+          <button name="6" onClick={e => onClick(e.target.name)}>6</button>
           <button name="*" onClick={e => onClick(e.target.name)}>*</button><br/>
 
-          <button name="." onClick={e => onClick(e.target.name)}>.</button>
+          <button name="1" onClick={e => onClick(e.target.name)}>1</button>
+          <button name="2" onClick={e => onClick(e.target.name)}>2</button>
+          <button name="3" onClick={e => onClick(e.target.name)}>3</button>
+          <button name="-" onClick={e => onClick(e.target.name)}>-</button><br/>
+
           <button name="0" onClick={e => onClick(e.target.name)}>0</button>
+          <button name="." onClick={e => onClick(e.target.name)}>.</button>
           <button name="=" onClick={e => onClick(e.target.name)}>=</button>
-          <button name="/" onClick={e => onClick(e.target.name)}>÷</button><br/>
+          <button name="+" onClick={e => onClick(e.target.name)}>+</button><br/>
       </div>
   );
 };
+
+
+//components to be tested
+export{
+  Result,
+  KeyArea
+}
