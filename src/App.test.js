f90e7c473f31b19fb7c5961044ba6a587912ff9e
Ian Kahwai <iankahwai@gmail.com>
2018-12-23 11:28:16 +0200

    Snapshot tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, { Result, KeyArea } from './App';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', ()=>{
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', ()=>{
+    const component = renderer.create(<App />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+});
+
+describe('Result', ()=>{
+  it('renderes without crashing', ()=>{
+    const div = document.createElement('div');
+    ReactDOM.render(<Result />, div);
+  });
+
+  test('has a valid snapshot', ()=>{
+    const component = renderer.create(<Result />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('KeyArea', ()=>{
+  it('renders without crashing', ()=>{
+    const div = document.createElement('div');
+    ReactDOM.render(<KeyArea />, div);
+  });
+
+  test('has a valid snapshot', ()=>{
+    const component = renderer.create(<KeyArea />);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
